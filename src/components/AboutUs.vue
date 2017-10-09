759f8b9b8aeca72ad32dd370c0b84dd50ee6a767
iggy <625376618@qq.com>
2017-10-09 21:19:27 +0800

    增加广告页面

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,60 +1,27 @@
 <template>
-  <v-card>
-    <search></search>
-    <v-container class="alert">
-      <v-alert error value="true" 
-        style="height:100px" v-model="showAlert">
-        错误请求.
-      </v-alert>
-    </v-container>
-    <v-container class="cart">
-	  <div class="text-xs-center">
-	    <v-icon large v-badge="{ value: 1, left: true}" class="grey--text text--lighten-1">shopping_cart</v-icon>
-
-	    <v-icon large v-badge="{ value: '!' }" class="grey--text red--after">mail</v-icon>
-	  </div>
-	</v-container>
-  </div>
-  <v-btn @click.native="get()"></v-btn>
-  <div> 
-  {{myMessage}}
-  </div>
-  </v-card>
+  <v-container grid-list-xl text-xs-center>
+    <div class="video">
+     <embed src="http://player.youku.com/player.php/sid/XMzA0OTY1OTY1Mg=/v.swf" mce_src="http://player.youku.com/player.php/sid/XMzA0OTY1OTY1Mg=/v.swf" quality="high" width="650" height="520"  align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash"
+            allowFullScreen='true'></embed>
+    </div>
+  </v-container>
 </template>
-
 <script>
-import Search from './Search'
-import axios from 'axios'
+
 export default {
   name: 'aboutus',
-  components: { Search },
   data () {
     return {
-      showAlert: false,
-      myMessage: '没有数据传输'
-
+      test: 'ss'
     }
   },
   methods: {
-    get () {         // 报415错误
-      axios.get('goods/type/selectAll.do ')
-      .then(function (res) {
-        this.myMessage = res.data.msg
-      }.bind(this)).catch(function (err) {
-        console.log(err)
-      })
-    }
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.alert{
-	width: 300px;
-	margin-right: 10px;
-}
-.cart{
-	margin-bottom: 15px
-}
+
 </style>
