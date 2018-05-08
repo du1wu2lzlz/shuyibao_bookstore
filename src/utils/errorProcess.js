557308f150020e3831feb7d32468e8ab4750c860
luke <625376618@qq.com>
2018-05-08 10:38:17 +0800

    未登陆用户 重定向至登陆页面 并提示信息

--- src/utils/errorProcess.js
+++ src/utils/errorProcess.js
@@ -1,5 +1,5 @@
 import Router from 'vue-router'
-
+import { Notification  } from 'element-ui';
 const router = new Router()
 
 export default function (data) {
@@ -13,6 +13,14 @@ export default function (data) {
   // 401用户未登录，重定向到登录界面
   if (data.status === 401) {
     router.push('login')
+    Notification({
+      title: '友情提示',
+      message: '请您先登陆哦',
+      type: 'warning',
+      duration: 2000,
+      position:'top-right',
+      offset: 30
+    })
     return
   }
 
